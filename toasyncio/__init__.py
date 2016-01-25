d3f972b0ff8779d3f2ee0a542b26d3c584595ca9
Dmitry Orlov <me@mosquito.su>
2016-01-25 18:42:34 +0300

    [fix] bump

--- toasyncio/__init__.py
+++ toasyncio/__init__.py
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-version_info = (0, 5, 2)
+version_info = (0, 5, 3)
 author_info = (
     {"name": "Ilya Smirnov", "email": "ilya94@mail.ru"},
     {"name": "Dmitry Orlov", "email": "me@mosquito.su"},
